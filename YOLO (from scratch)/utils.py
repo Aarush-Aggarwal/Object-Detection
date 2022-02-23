fd0d1ff5816427c60a71f38a85f4ad7a1feddcbb
Aarush Aggarwal <46350653+Aarush-Aggarwal@users.noreply.github.com>
2022-02-24 00:16:25 +0530

    Update utils.py

--- YOLO (from scratch)/utils.py
+++ YOLO (from scratch)/utils.py
@@ -48,3 +48,39 @@ def IoU(boxes_preds, boxes_labels, boxes_format="midpoint"):
     box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
 
     return intersection / (box1_area + box2_area - intersection + 1e-6)
+
+
+def NMS(bboxes, iou_threshold, prob, boxes_format="corners"):
+    """
+    Non Max Suppression
+    
+    Parameters:
+        bboxes (list): list of lists 
+        containing all bboxes with each bboxes specified as [class_pred, prob_score, x1, y1, x2, y2]
+        
+        iou_threshold (float): threshold where predicted bboxes is correct
+        
+        threshold (float): threshold to remove predicted bboxes (independent of IoU) 
+        
+        box_format (str): "midpoint" or "corners" used to specify bboxes
+        
+    Returns:
+        list: bboxes after performing NMS given a specific IoU threshold
+    """
+    assert type(bboxes) == list
+    
+    bboxes = [box for box in bboxes if box[1] > prob]
+    bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
+    
+    bboxes_after_nms = []
+    
+    while bboxes:
+        highest_prob_bbox = bboxes.pop(0)
+        bboxes_after_nms.append(highest_prob_bbox)
+        
+        bboxes = [box for box in bboxes 
+                  if box[0] != highest_prob_bbox[0] 
+                  or IoU(torch.tensor(box[2:]), torch.tensor(highest_prob_bbox[2:]))
+                  ]
+        
+    return bboxes_after_nms
